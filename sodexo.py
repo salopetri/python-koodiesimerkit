455c6292e192c51d35b53825f029bf3ad02fc157
Petri Salo <petri.salo@hamk.fi>
2019-09-20 10:39:13 +0300

    Sodexo ruokalista esimerkki 20.9.2019

---
+++ sodexo.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from datetime import date
+
+restaurants = {"Visamäki" : "31314", "Riihimäki" : "31332", "Valkeakoski" : "31334"}
+restaurant = None
+
+while not restaurant:
+    print("""SODEXO RUOKALISTA
+    1) Visamäki
+    2) Riihimäki
+    3) Valkeakoski""")
+    valinta = input("Valintasi: ")
+
+    if (valinta == "1" or valinta.lower() == "visamäki"):
+        restaurant = restaurants["Visamäki"]
+    elif (valinta == "2" or valinta.lower() == "riihimäki"):
+        restaurant = restaurants["Riihimäki"]
+    elif (valinta == "3" or valinta.lower() == "valkeakoski"):
+        restaurant = restaurants["Valkeakoski"]
+    else:
+        print("Virheellinen valinta. Yritä uudelleen!")
+
+pvm = date.today()
+year = str(pvm.year)
+month = str(pvm.month)
+day = str(pvm.day)
+
+data = requests.get("https://www.sodexo.fi/ruokalistat/output/daily_json/"+restaurant+"/"+year+"/"+month+"/"+day+"/fi")
+ruokalista = json.loads(data.text)
+
+ruokalajit = ruokalista["courses"]
+
+for ruoka in ruokalajit:
+    print(ruoka["title_fi"])
+
