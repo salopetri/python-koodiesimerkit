bf756ebdaeab7f8904f05cc0682a89e28231cbab
Petri Salo <petri.salo@hamk.fi>
2019-10-07 14:39:19 +0300

    Laskuri välkkyy viimeisen minuutin aikana

--- Viikko 7/countdown.py
+++ Viikko 7/countdown.py
@@ -20,24 +20,34 @@ def start(param = None):
         """
         Tässä funktiossa tapahtuu varsinainen kellon toiminta
         """
-        laskuri.grid(row=3, column=0)
         # Luetaan käyttäjän syöte datetime-objektiksi
         target = datetime.strptime(kohdeInput.get(), "%H:%M:%S")
+        diff = target-datetime.now()
+        last = time.time()
+        blink = False
         while True:
-                # Lasketaan timedelta nykyhetken ja kohteen välillä
-                diff = target-datetime.now()
-                # Haetaan timedeltan sekunnit
-                s = diff.seconds
-                # Lasketaan tunnit
-                h = formatTime(int(s/3600))
-                # Vähennetään tunnit sekunneista laskemalla jakojäännös
-                s %= 3600
-                # Lasketaan minuutit
-                m = formatTime(int(s/60))
-                # Lasketaan jäljelle jäävät sekunnit
-                s = formatTime(int(s%60))
-                # Päivitetään laskurin teksti
-                laskuri.config(text="{0}:{1}:{2}".format(h,m,s))
+                # Päivitetään kello, jos edellisestä päivityksestä on kulunut sekunti
+                if (time.time() >= last+1):
+                        # Lasketaan timedelta nykyhetken ja kohteen välillä
+                        diff = target-datetime.now()
+                        # Haetaan timedeltan sekunnit
+                        s = diff.seconds
+                        # Lasketaan tunnit
+                        h = formatTime(int(s/3600))
+                        # Vähennetään tunnit sekunneista laskemalla jakojäännös
+                        s %= 3600
+                        # Lasketaan minuutit
+                        m = formatTime(int(s/60))
+                        # Lasketaan jäljelle jäävät sekunnit
+                        s = formatTime(int(s%60))
+                        # Päivitetään laskurin teksti
+                        laskuri.config(text="{0}:{1}:{2}".format(h,m,s), fg='white')
+                        blink = False
+                        last = time.time()
+                # Laskuri välkkyy punaisena viimeisen minuutin aikana
+                if (time.time()-last >= 0.5 and not blink and diff.seconds < 60):
+                        laskuri.config(fg='red')
+                        blink = True
                 ikkuna.update()
                 time.sleep(0.01)
 
@@ -49,13 +59,14 @@ ohjeteksti = Label(ikkuna, text="Anna kohdeaika muodossa HH:MM:SS")
 ohjeteksti.grid(row=0, column=0, pady=20, padx=100)
 
 kohdeInput = Entry(ikkuna, width=30)
-kohdeInput.grid(row=1, column=0, pady=20, padx=100)
+kohdeInput.grid(row=1, column=0, pady=10, padx=100)
 kohdeInput.bind("<Return>", start)
 kohdeInput.focus()
 
 aloitusNappi = Button(ikkuna, text="Aloita", command=start)
-aloitusNappi.grid(row=2, column=0)
+aloitusNappi.grid(row=2, column=0, pady=20)
 
-laskuri = Label(ikkuna, text="00:00:00", width=20, pady=20, font=("Consolas", 50), fg='white', bg='black')
+laskuri = Label(ikkuna, text="00:00:00", width=20, pady=20, font=("Consolas", 72), fg='white', bg='black')
+laskuri.grid(row=3, column=0)
 
 ikkuna.mainloop()
