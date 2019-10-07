abeeb2a40a67861226e21e59a14ae4157c33312b
Petri Salo <petri.salo@hamk.fi>
2019-10-07 14:22:30 +0300

    Korjattu sisennysvirhe

--- Viikko 7/countdown.py
+++ Viikko 7/countdown.py
@@ -7,39 +7,39 @@ import time
 from tkinter import *
 
 def formatTime(time):
-"""
-Tämä funktio lisää numeron alkuun ylimääräisen nollan,
-jos luku on alle 10 ja muuntaa sen samalla merkkijonoksi
-"""
-    if (time<10):
-        return "0"+str(time)
-    else:
-        return str(time)
+        """
+        Tämä funktio lisää numeron alkuun ylimääräisen nollan,
+        jos luku on alle 10 ja muuntaa sen samalla merkkijonoksi
+        """
+        if (time<10):
+                return "0"+str(time)
+        else:
+                return str(time)
 
 def start(param = None):
-"""
-Tässä funktiossa tapahtuu varsinainen kellon toiminta
-"""
-    laskuri.grid(row=3, column=0)
-    # Luetaan käyttäjän syöte datetime-objektiksi
-    target = datetime.strptime(kohdeInput.get(), "%H:%M:%S")
-    while True:
-        # Lasketaan timedelta nykyhetken ja kohteen välillä
-        diff = target-datetime.now()
-        # Haetaan timedeltan sekunnit
-        s = diff.seconds
-        # Lasketaan tunnit
-        h = formatTime(int(s/3600))
-        # Vähennetään tunnit sekunneista laskemalla jakojäännös
-        s %= 3600
-        # Lasketaan minuutit
-        m = formatTime(int(s/60))
-        # Lasketaan jäljelle jäävät sekunnit
-        s = formatTime(int(s%60))
-        # Päivitetään laskurin teksti
-        laskuri.config(text="{0}:{1}:{2}".format(h,m,s))
-        ikkuna.update()
-        time.sleep(0.01)
+        """
+        Tässä funktiossa tapahtuu varsinainen kellon toiminta
+        """
+        laskuri.grid(row=3, column=0)
+        # Luetaan käyttäjän syöte datetime-objektiksi
+        target = datetime.strptime(kohdeInput.get(), "%H:%M:%S")
+        while True:
+                # Lasketaan timedelta nykyhetken ja kohteen välillä
+                diff = target-datetime.now()
+                # Haetaan timedeltan sekunnit
+                s = diff.seconds
+                # Lasketaan tunnit
+                h = formatTime(int(s/3600))
+                # Vähennetään tunnit sekunneista laskemalla jakojäännös
+                s %= 3600
+                # Lasketaan minuutit
+                m = formatTime(int(s/60))
+                # Lasketaan jäljelle jäävät sekunnit
+                s = formatTime(int(s%60))
+                # Päivitetään laskurin teksti
+                laskuri.config(text="{0}:{1}:{2}".format(h,m,s))
+                ikkuna.update()
+                time.sleep(0.01)
 
 ikkuna = Tk()
 ikkuna.title("Countdown timer")
